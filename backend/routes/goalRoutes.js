f26141947383aa794a9c7866ff48e49a9ea192b4
bejoi <obedindevu@gmail.com>
2023-03-11 22:19:28 +0200

    create server

---
+++ backend/routes/goalRoutes.js
@@ -0,0 +1,10 @@
+const express = require("express")
+const router = express.Router()
+
+router.get("/", (req, res) => {
+    res.status(200).json({
+        message:"suis tres sur de moi meme"
+    })
+})
+
+module.exports = router
