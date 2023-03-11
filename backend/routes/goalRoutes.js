2fdeafe2cb8b55f9110dddd6557cae9163e31ec0
bejoi <obedindevu@gmail.com>
2023-03-11 22:20:32 +0200

    create routers

--- backend/routes/goalRoutes.js
+++ backend/routes/goalRoutes.js
@@ -6,5 +6,9 @@ router.get("/", (req, res) => {
         message:"suis tres sur de moi meme"
     })
 })
-
+router.post("/", (req, res) => {
+    res.status(200).json({
+        message:"suis tres sur de moi meme"
+    })
+})
 module.exports = router
