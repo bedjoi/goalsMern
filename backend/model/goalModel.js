22cb1af168c95306d3a9061dc21311f64c7ec8d7
bejoi <obedindevu@gmail.com>
2023-04-24 16:58:35 +0200

    feat: create agoal schema

---
+++ backend/model/goalModel.js
@@ -0,0 +1,14 @@
+const mongoose = require('mongoose');
+
+const goalSchema = mongoose.Schema(
+	{
+		text: {
+			type: string,
+			required: [true, 'Please add a text value'],
+		},
+	},
+	{
+		timestamps: true,
+	}
+);
+module.exports = mongoose.model('goal', goalSchema);
