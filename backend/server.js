d36ccb159225e7aa00fc245e48e6cce33d28deb3
bejoi <obedindevu@gmail.com>
2023-04-24 16:38:54 +0200

    feat: connect to mongo db

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,11 @@
 const express = require('express');
+const colors = require('colors');
 const dotenv = require('dotenv').config();
 const { errorHandler } = require('./midleware/errorMidleware');
+const connectDB = require('./config/db');
 const port = process.env.PORT || 8000;
+
+connectDB();
 
 const app = express();
 
