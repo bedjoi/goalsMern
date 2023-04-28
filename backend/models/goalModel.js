578c39be581a20b8e8adc00e9f7e2513ec141bc8
bejoi <obedindevu@gmail.com>
2023-04-28 11:53:24 +0200

    feat:crete the goalSchema

--- backend/models/goalModel.js
+++ backend/models/goalModel.js
@@ -3,7 +3,7 @@ const mongoose = require('mongoose');
 const goalSchema = mongoose.Schema(
 	{
 		text: {
-			type: string,
+			type: String,
 			required: [true, 'Please add a text value'],
 		},
 	},
